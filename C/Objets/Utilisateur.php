68b6e3d92d3c78c5d73da6d3e9d23dad1aaf9616
Mel-meel <mel.avizou@mail.ru>
2023-12-20 13:46:17 +0100

    Ajout getters et setters classe Utilisateur

--- C/Objets/Utilisateur.php
+++ C/Objets/Utilisateur.php
@@ -6,4 +6,24 @@ class Utilisateur {
         private string $id ;
         private string $nom ;
     ) {}
+    
+    /**
+     * Getters et Setters
+     */
+     
+     public function get_id() : int {
+         return $this->id ;
+     }
+     
+     public function get_nom() : string {
+         return $this->nom ;
+     }
+     
+     public function set_id(int $id) {
+         $this->id = $id ;
+     }
+     
+     public function set_nom(string $nom) {
+         $this->nom = $nom ;
+     }
 }
