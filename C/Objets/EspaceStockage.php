4115f1bf10761187a3d43bcc5f74af63faee112f
Mel-meel <mel.avizou@mail.ru>
2023-12-20 12:58:53 +0100

    Getters et setter de EspaceStockage

--- C/Objets/EspaceStockage.php
+++ C/Objets/EspaceStockage.php
@@ -7,8 +7,52 @@ class EspaceStockage {
     public function __construct(
         private int $id ;
         private string $nom ;
-        private string $taille_allouee ;
+        private int $taille_allouee ;
         private Groupe $proprietaires ;
         private array $configurations ;
     ) {}
+    
+    /**
+     * Getters et Setters
+     */
+     
+     public function get_id() : int {
+         return $this->id ;
+     }
+     
+     public function get_nom() : string {
+         return $this->nom ;
+     }
+     
+     public function get_taille_allouee() : int {
+         return $this->taille_allouee ;
+     }
+     
+     public function get_proprietaires() : Groupe {
+         return $this->proprietaires ;
+     }
+     
+     public function get_configurations() : array {
+         return $this->configurations ;
+     }
+     
+     public function set_id(int $id) {
+         $this->id = $id ;
+     }
+     
+     public function set_nom(string $nom) {
+         $this->nom = $nom ;
+     }
+     
+     public function set_taille_allouee(int $taille_allouee) {
+         $this->taille_allouee = $taille_allouee ;
+     }
+     
+     public function set_taille_allouee(Groupe $proprietaires) {
+         $this->proprietaires = $proprietaires ;
+     }
+     
+     public function set_taille_allouee(array $configurations) {
+         $this->configurations = $configurations ;
+     }
 }
